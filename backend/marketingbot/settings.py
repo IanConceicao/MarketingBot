c51f7724ac742226906acfc0ebe6bc2d3e6548d6
IanConceicao <IanCon234@gmail.com>
2023-05-02 20:28:14 -0700

    Static files trying to fix

--- backend/marketingbot/settings.py
+++ backend/marketingbot/settings.py
@@ -135,9 +135,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 MEDIA_URL = "images/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "staticfiles"]
 
 MEDIA_ROOT = BASE_DIR / "static/images"
 STATIC_ROOT = BASE_DIR / "staticfiles"
