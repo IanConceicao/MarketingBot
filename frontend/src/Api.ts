cb3760e55f5f4e3ac7a2bd184d2b0c63a4dc40e6
IanConceicao <IanCon234@gmail.com>
2023-05-02 21:30:48 -0700

    New models, load starter models slight change to API string

--- frontend/src/Api.ts
+++ frontend/src/Api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
-const LOCAL_URL = "http://localhost:8000"
-let BACKEND_URL = (process.env.BACKEND_URL || LOCAL_URL) + "/api/"
+const LOCAL_URL = "http://localhost:8000/"
+let BACKEND_URL = (process.env.BACKEND_URL || LOCAL_URL) + "api/"
 const headers = { "Content-Type": "application/json" }
 
 function handleErrors(error: any) {
